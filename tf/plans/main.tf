a2930ec17a5f5a95d1a22cde997da2e5dcc3d710
Boris Quiroz <bmquiroz@outlook.com>
2024-03-17 13:42:46 -0400

    ...

--- tf/plans/main.tf
+++ tf/plans/main.tf
@@ -7,11 +7,15 @@ provider "aws" {
 #   token = var.pd-secret-value
 # }
 
+locals {
+  pd_secret_value = jsondecode(data.aws_secretsmanager_secret_version.pd-secret-version.secret_string)["pd-secret"]
+}
+
 module "pd-common" {
   source                      = "../common-modules"
   rotation_start              = "2017-06-01T12:00:00-04:00"
   rotation_virtual_start      = "2017-06-01T12:00:00-04:00"
-  pd_secret_value             = pd-secret-value
+  pd_secret_value             = local.pd_secret_value
 
   # depends_on = [
   #   module.base-infra
@@ -22,7 +26,7 @@ module "pd-gbst-prod" {
   source                      = "../app-modules/gbst"
   environment                 = "prod"
   pd_gbst_ep_user_email       = "bmquiroz@rc-its.com"
-  pd_secret_value             = pd-secret-value
+  pd_secret_value             = local.pd_secret_value
 
   # depends_on = [
   #   module.pd-common
