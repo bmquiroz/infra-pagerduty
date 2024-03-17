f57f19c01411cc5ffe68e1b63481b5ecef1f8af1
Boris Quiroz <bmquiroz@outlook.com>
2024-03-17 11:31:06 -0400

    ...

--- tf/app-modules/gbst/main.tf
+++ tf/app-modules/gbst/main.tf
@@ -1,11 +1,11 @@
-terraform {
-  required_providers {
-    pagerduty = {
-      source  = "pagerduty/pagerduty"
-      version = "2.2.1"
-    }
-  }
-}
+# terraform {
+#   required_providers {
+#     pagerduty = {
+#       source  = "pagerduty/pagerduty"
+#       version = "2.2.1"
+#     }
+#   }
+# }
 
 provider "pagerduty" {
   token = var.pd-secret-value
