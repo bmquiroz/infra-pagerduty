0a4278ddf501708cb33a0853e69b0023393b5c54
Boris Quiroz <bmquiroz@outlook.com>
2024-03-17 14:39:04 -0400

    ...

--- tf/plans/output.tf
+++ tf/plans/output.tf
@@ -3,6 +3,45 @@
 #   sensitive = true
 # }
 
-# output "gps-datadog-integration-key" {
-#   value = pagerduty_service_integration.prod-gps-service-integration-datadog.integration_key
-# }
+output "gps-datadog-integration-id" {
+  value = module.pd-gbst-prod.gps-datadog-integration-id
+}
+
+output "gps-datadog-integration-key" {
+  value = module.pd-gbst-prod.gps-datadog-integration-key
+}
+
+output "brd-datadog-integration-id" {
+  value = module.pd-gbst-prod.brd-datadog-integration-id
+}
+
+output "brd-datadog-integration-key" {
+  value = module.pd-gbst-prod.brd-datadog-integration-key
+}
+
+# DEB outputs
+output "deb-datadog-integration-id" {
+  value = module.pd-gbst-prod.deb-datadog-integration-id
+}
+
+output "deb-datadog-integration-key" {
+  value = module.pd-gbst-prod.deb-datadog-integration-key
+}
+
+# JAG outputs
+output "jag-datadog-integration-id" {
+  value = module.pd-gbst-prod.jag-datadog-integration-id
+}
+
+output "jag-datadog-integration-key" {
+  value = module.pd-gbst-prod.jag-datadog-integration-key
+}
+
+# PPL outputs
+output "ppl-datadog-integration-id" {
+  value = module.pd-gbst-prod.ppl-datadog-integration-id
+}
+
+output "ppl-datadog-integration-key" {
+  value = module.pd-gbst-prod.ppl-datadog-integration-key
+}
