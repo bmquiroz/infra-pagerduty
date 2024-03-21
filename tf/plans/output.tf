addbf9810375c0986a63c1b124c4cfcc15ff6f8b
Boris Quiroz <bmquiroz@outlook.com>
2024-03-21 18:04:47 -0400

    ...

--- tf/plans/output.tf
+++ tf/plans/output.tf
@@ -44,148 +44,148 @@ output "ppl-datadog-integration-key" {
   value = module.pd-gbst-prod.ppl-datadog-integration-key
 }
 
-## SMRS
-# ARI outputs
-output "ari-datadog-integration-id" {
-  value = module.pd-smrs-prod.ari-datadog-integration-id
-}
+# ## SMRS
+# # ARI outputs
+# output "ari-datadog-integration-id" {
+#   value = module.pd-smrs-prod.ari-datadog-integration-id
+# }
 
-output "ari-datadog-integration-key" {
-  value = module.pd-smrs-prod.ari-datadog-integration-key
-}
+# output "ari-datadog-integration-key" {
+#   value = module.pd-smrs-prod.ari-datadog-integration-key
+# }
 
-# CTR outputs
-output "ctr-datadog-integration-id" {
-  value = module.pd-smrs-prod.ctr-datadog-integration-id
-}
+# # CTR outputs
+# output "ctr-datadog-integration-id" {
+#   value = module.pd-smrs-prod.ctr-datadog-integration-id
+# }
 
-output "ctr-datadog-integration-key" {
-  value = module.pd-smrs-prod.ctr-datadog-integration-key
-}
+# output "ctr-datadog-integration-key" {
+#   value = module.pd-smrs-prod.ctr-datadog-integration-key
+# }
 
-# EMA outputs
-output "ema-datadog-integration-id" {
-  value = module.pd-smrs-prod.ema-datadog-integration-id
-}
+# # EMA outputs
+# output "ema-datadog-integration-id" {
+#   value = module.pd-smrs-prod.ema-datadog-integration-id
+# }
 
-output "ema-datadog-integration-key" {
-  value = module.pd-smrs-prod.ema-datadog-integration-key
-}
+# output "ema-datadog-integration-key" {
+#   value = module.pd-smrs-prod.ema-datadog-integration-key
+# }
 
-# SCR outputs
-output "scr-datadog-integration-id" {
-  value = module.pd-smrs-prod.scr-datadog-integration-id
-}
+# # SCR outputs
+# output "scr-datadog-integration-id" {
+#   value = module.pd-smrs-prod.scr-datadog-integration-id
+# }
 
-output "scr-datadog-integration-key" {
-  value = module.pd-smrs-prod.scr-datadog-integration-key
-}
+# output "scr-datadog-integration-key" {
+#   value = module.pd-smrs-prod.scr-datadog-integration-key
+# }
 
-# SST outputs
-output "sst-datadog-integration-id" {
-  value = module.pd-smrs-prod.sst-datadog-integration-id
-}
+# # SST outputs
+# output "sst-datadog-integration-id" {
+#   value = module.pd-smrs-prod.sst-datadog-integration-id
+# }
 
-output "sst-datadog-integration-key" {
-  value = module.pd-smrs-prod.sst-datadog-integration-key
-}
+# output "sst-datadog-integration-key" {
+#   value = module.pd-smrs-prod.sst-datadog-integration-key
+# }
 
-# STR outputs
-output "str-datadog-integration-id" {
-  value = module.pd-smrs-prod.str-datadog-integration-id
-}
+# # STR outputs
+# output "str-datadog-integration-id" {
+#   value = module.pd-smrs-prod.str-datadog-integration-id
+# }
 
-output "str-datadog-integration-key" {
-  value = module.pd-smrs-prod.str-datadog-integration-key
-}
+# output "str-datadog-integration-key" {
+#   value = module.pd-smrs-prod.str-datadog-integration-key
+# }
 
-# APT outputs
-output "apt-datadog-integration-id" {
-  value = module.pd-smrs-prod.apt-datadog-integration-id
-}
+# # APT outputs
+# output "apt-datadog-integration-id" {
+#   value = module.pd-smrs-prod.apt-datadog-integration-id
+# }
 
-output "apt-datadog-integration-key" {
-  value = module.pd-smrs-prod.apt-datadog-integration-key
-}
+# output "apt-datadog-integration-key" {
+#   value = module.pd-smrs-prod.apt-datadog-integration-key
+# }
 
-## DMG
-# AEX outputs
-output "aex-datadog-integration-id" {
-  value = module.pd-dmg-prod.aex-datadog-integration-id
-}
+# ## DMG
+# # AEX outputs
+# output "aex-datadog-integration-id" {
+#   value = module.pd-dmg-prod.aex-datadog-integration-id
+# }
 
-output "aex-datadog-integration-key" {
-  value = module.pd-dmg-prod.aex-datadog-integration-key
-}
+# output "aex-datadog-integration-key" {
+#   value = module.pd-dmg-prod.aex-datadog-integration-key
+# }
 
-# BCM outputs
-output "bcm-datadog-integration-id" {
-  value = module.pd-dmg-prod.bcm-datadog-integration-id
-}
+# # BCM outputs
+# output "bcm-datadog-integration-id" {
+#   value = module.pd-dmg-prod.bcm-datadog-integration-id
+# }
 
-output "bcm-datadog-integration-key" {
-  value = module.pd-dmg-prod.bcm-datadog-integration-key
-}
+# output "bcm-datadog-integration-key" {
+#   value = module.pd-dmg-prod.bcm-datadog-integration-key
+# }
 
-# BSM outputs
-output "bsm-datadog-integration-id" {
-  value = module.pd-dmg-prod.bsm-datadog-integration-id
-}
+# # BSM outputs
+# output "bsm-datadog-integration-id" {
+#   value = module.pd-dmg-prod.bsm-datadog-integration-id
+# }
 
-output "bsm-datadog-integration-key" {
-  value = module.pd-dmg-prod.bsm-datadog-integration-key
-}
+# output "bsm-datadog-integration-key" {
+#   value = module.pd-dmg-prod.bsm-datadog-integration-key
+# }
 
-# CDR outputs
-output "cdr-datadog-integration-id" {
-  value = module.pd-dmg-prod.cdr-datadog-integration-id
-}
+# # CDR outputs
+# output "cdr-datadog-integration-id" {
+#   value = module.pd-dmg-prod.cdr-datadog-integration-id
+# }
 
-output "cdr-datadog-integration-key" {
-  value = module.pd-dmg-prod.cdr-datadog-integration-key
-}
+# output "cdr-datadog-integration-key" {
+#   value = module.pd-dmg-prod.cdr-datadog-integration-key
+# }
 
-# IND outputs
-output "ind-datadog-integration-id" {
-  value = module.pd-dmg-prod.ind-datadog-integration-id
-}
+# # IND outputs
+# output "ind-datadog-integration-id" {
+#   value = module.pd-dmg-prod.ind-datadog-integration-id
+# }
 
-output "ind-datadog-integration-key" {
-  value = module.pd-dmg-prod.ind-datadog-integration-key
-}
+# output "ind-datadog-integration-key" {
+#   value = module.pd-dmg-prod.ind-datadog-integration-key
+# }
 
-# RSH outputs
-output "rsh-datadog-integration-id" {
-  value = module.pd-dmg-prod.rsh-datadog-integration-id
-}
+# # RSH outputs
+# output "rsh-datadog-integration-id" {
+#   value = module.pd-dmg-prod.rsh-datadog-integration-id
+# }
 
-output "rsh-datadog-integration-key" {
-  value = module.pd-dmg-prod.rsh-datadog-integration-key
-}
+# output "rsh-datadog-integration-key" {
+#   value = module.pd-dmg-prod.rsh-datadog-integration-key
+# }
 
-# SHM outputs
-output "shm-datadog-integration-id" {
-  value = module.pd-dmg-prod.shm-datadog-integration-id
-}
+# # SHM outputs
+# output "shm-datadog-integration-id" {
+#   value = module.pd-dmg-prod.shm-datadog-integration-id
+# }
 
-output "shm-datadog-integration-key" {
-  value = module.pd-dmg-prod.shm-datadog-integration-key
-}
+# output "shm-datadog-integration-key" {
+#   value = module.pd-dmg-prod.shm-datadog-integration-key
+# }
 
-# SMD outputs
-output "smd-datadog-integration-id" {
-  value = module.pd-dmg-prod.smd-datadog-integration-id
-}
+# # SMD outputs
+# output "smd-datadog-integration-id" {
+#   value = module.pd-dmg-prod.smd-datadog-integration-id
+# }
 
-output "smd-datadog-integration-key" {
-  value = module.pd-dmg-prod.smd-datadog-integration-key
-}
+# output "smd-datadog-integration-key" {
+#   value = module.pd-dmg-prod.smd-datadog-integration-key
+# }
 
-# RPA outputs
-output "rpa-datadog-integration-id" {
-  value = module.pd-dmg-prod.rpa-datadog-integration-id
-}
+# # RPA outputs
+# output "rpa-datadog-integration-id" {
+#   value = module.pd-dmg-prod.rpa-datadog-integration-id
+# }
 
-output "rpa-datadog-integration-key" {
-  value = module.pd-dmg-prod.rpa-datadog-integration-key
-}
+# output "rpa-datadog-integration-key" {
+#   value = module.pd-dmg-prod.rpa-datadog-integration-key
+# }
