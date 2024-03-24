f1db6d17d425a9f874e9acfe09e042a2cf52f860
Boris Quiroz <bmquiroz@outlook.com>
2024-03-24 17:18:02 -0400

    ...

--- tf/common-modules/output.tf
+++ tf/common-modules/output.tf
@@ -13,3 +13,27 @@ output "pd-db-gbst-schedule-id" {
 output "pd-app-gbst-schedule-id" {
   value = pagerduty_schedule.app-sev1-gbst-schedule.id
 }
+
+output "pd-smrs-schedule-id" {
+  value = pagerduty_schedule.techdevsecops-sev1-smrs-schedule.id
+}
+
+output "pd-db-smrs-schedule-id" {
+  value = pagerduty_schedule.techdb-sev1-smrs-schedule.id
+}
+
+output "pd-app-smrs-schedule-id" {
+  value = pagerduty_schedule.app-sev1-smrs-schedule.id
+}
+
+output "pd-dmg-schedule-id" {
+  value = pagerduty_schedule.techdevsecops-sev1-dmg-schedule.id
+}
+
+output "pd-db-dmg-schedule-id" {
+  value = pagerduty_schedule.techdb-sev1-dmg-schedule.id
+}
+
+output "pd-app-dmg-schedule-id" {
+  value = pagerduty_schedule.app-sev1-dmg-schedule.id
+}
